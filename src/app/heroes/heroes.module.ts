2ccd8c819232dcd726df7f551d45ac4e74aed775
Erick Estrada <erickestradapj@gmail.com>
2021-11-17 12:41:30 -0500

    heroes routing added

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+
 import { AddComponent } from './pages/add/add.component';
-import { SearchComponent } from './pages/search/search.component';
 import { HeroComponent } from './pages/hero/hero.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
+import { SearchComponent } from './pages/search/search.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,6 @@ import { ListComponent } from './pages/list/list.component';
     HomeComponent,
     ListComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, HeroesRoutingModule],
 })
 export class HeroesModule {}
