ba649d66fdcf49b50b5ab24eff1634f2c098f7c1
Erick Estrada <erickestradapj@gmail.com>
2021-11-18 11:48:19 -0500

    environments added

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/heroes.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styles: [],
+  styles: [
+    `
+      img {
+        width: 100%;
+        border-radius: 5px;
+      }
+    `,
+  ],
 })
 export class HeroComponent implements OnInit {
-  constructor(private activatedRoute: ActivatedRoute) {}
+  hero!: Hero;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({ id }) => console.log(id));
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => (this.hero = hero));
+  }
+
+  // Another way to do it
+  public goBack() {
+    this.router.navigate(['/heroes/list']);
   }
 }
